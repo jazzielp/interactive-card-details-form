7fe312108c11765d780ec24366e598ce06b85bb0
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-21 11:56:48 -0700

    Create ccs of the component FrontCard

--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -1,12 +1,13 @@
+import './FrontCard.css'
+import IconCard from '../icons/card-logo.svg'
 export const FrontCard = () => {
   return (
     <div className='front-card'>
       <div className='front-card__icon-container'>
-        <img className='front-card__icon-primary' src='../icons/card-logo.svg' alt='Icon card' />
-        <div className='front-card__icon-second' />
+        <img className='front-card__icon-primary' src={IconCard} alt='Icon card' />
       </div>
       <div className='front-card__number-card-container'>
-        <p className='front-card__number-card'>0000 0000 0000 0000</p>
+        <p className='front-card__number-card'>9591  6589  6389  1011</p>
       </div>
       <div className='front-card__info-container'>
         <p className='front-card__name'>JANE APPLESEED</p>
