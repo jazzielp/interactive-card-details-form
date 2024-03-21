55c35b9eaf3a4a9e60db06d2f496ea43e9a8397c
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-21 12:10:24 -0700

    Create BackCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { FrontCard } from './components/FrontCard'
+import { BackCard } from './components/BackCard'
 export function App () {
   return (
     <>
       <FrontCard />
+      <BackCard />
     </>
   )
 }
---
+++ src/components/BackCard.jsx
@@ -0,0 +1,8 @@
+import './Backcard.css'
+export const BackCard = () => {
+  return (
+    <div className='back-card'>
+      <p className='back-card__security-code'>123</p>
+    </div>
+  )
+}
