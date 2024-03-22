0f5dab49c2d4700c52d13f2e846fe2bc455060f5
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-22 15:13:22 -0700

    Add parameters in FrontCard component

--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -1,17 +1,18 @@
 import './FrontCard.css'
 import IconCard from '../icons/card-logo.svg'
-export const FrontCard = () => {
+export const FrontCard = ({ name, number = '0000 0000 0000 0000', month = '00', year = '00' }) => {
+  const upperName = name.toUpperCase()
   return (
     <div className='front-card'>
       <div className='front-card__icon-container'>
         <img className='front-card__icon-primary' src={IconCard} alt='Icon card' />
       </div>
       <div className='front-card__number-card-container'>
-        <p className='front-card__number-card'>9591  6589  6389  1011</p>
+        <p className='front-card__number-card'>{number}</p>
       </div>
       <div className='front-card__info-container'>
-        <p className='front-card__name'>JANE APPLESEED</p>
-        <p className='front-card__data'>00/00</p>
+        <p className='front-card__name'>{upperName}</p>
+        <p className='front-card__data'>{month}/{year}</p>
       </div>
     </div>
   )
