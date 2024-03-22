1ed4b3b772245b4d6d5260acfe06e7d57545dcb3
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-22 15:13:45 -0700

    Past the object

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import { FrontCard } from './FrontCard'
 import { BackCard } from './BackCard'
 import './Header.css'
-export const Header = () => {
+export const Header = ({ dataCard }) => {
+  const { name, number, month, year, code } = dataCard
   return (
     <header className='header'>
       <div className='header__card-container'>
         <div className='header__frontCard-container'>
-          <BackCard />
+          <BackCard code={code} />
         </div>
         <div className='header__backCard-container'>
-          <FrontCard />
+          <FrontCard name={name} number={number} month={month} year={year} />
         </div>
       </div>
     </header>
