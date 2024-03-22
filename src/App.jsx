8cfe76dc1bc7e3c1e60429a8fc4a6863e3e717a3
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-22 12:59:39 -0700

    Add many chances

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { Header } from './components/Header'
 import { Form } from './components/Form'
 import { Thanks } from './components/Thanks'
+import { useState } from 'react'
 export function App () {
+  const cardJson = {
+    name: 'JANE APPLESEED',
+    number: '0000 0000 0000 0000',
+    month: '00',
+    year: '00',
+    code: '000'
+  }
+
+  const [isComplete, setIsComplete] = useState(false)
+  const [dataCard, setDataCard] = useState(cardJson)
+
+  const handleComplete = () => {
+    setIsComplete(!isComplete)
+  }
+
   return (
     <>
-      <Header />
+      <Header dataCard={dataCard} />
       <main>
-        <Form />
+        {isComplete ? <Thanks handleComplete={handleComplete} /> : <Form dataCard={dataCard} setDataCard={setDataCard} />}
       </main>
-      <Thanks />
     </>
   )
 }
