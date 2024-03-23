b6dafade802ebb10c37f5477a86e14241b225af8
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-23 10:05:49 -0700

    Update component to render object instead of text
    Modifies the component to accommodate changes in the data source, which now provides
    an object instead of a simple text value. Adjusts the rendering logic to access the
    appropriate property ('text') of the object for display, ensuring compatibility with
    the updated data structure.

--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -1,18 +1,18 @@
 import './FrontCard.css'
 import IconCard from '../icons/card-logo.svg'
 export const FrontCard = ({ name, number, month, year }) => {
-  const upperName = name.toUpperCase()
+  const upperName = name.text.toUpperCase()
   return (
     <div className='front-card'>
       <div className='front-card__icon-container'>
         <img className='front-card__icon-primary' src={IconCard} alt='Icon card' />
       </div>
       <div className='front-card__number-card-container'>
-        <p className='front-card__number-card'>{number}</p>
+        <p className='front-card__number-card'>{number.text}</p>
       </div>
       <div className='front-card__info-container'>
         <p className='front-card__name'>{upperName}</p>
-        <p className='front-card__data'>{month}/{year}</p>
+        <p className='front-card__data'>{month.text}/{year.text}</p>
       </div>
     </div>
   )
