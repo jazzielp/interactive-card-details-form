7f7046a9416435632f0f8bb4ad3870accc2b8ad7
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-23 09:48:30 -0700

    Implement character limit validation for user input
    Adds client-side validation to restrict the number of characters that users
    can input into the designated field. This ensures data integrity and prevents
    potential issues with excessively long input values.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,23 @@
 import './Form.css'
 import { ButtonPrincipal } from './ButtonPrincipal'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 export const Form = ({ dataCard, setDataCard }) => {
-  const [dataCardRender, setDataCardRender] = useState()
+  const [dataCardRender, setDataCardRender] = useState({
+    name: '',
+    number: '',
+    month: '',
+    year: '',
+    code: ''
+  })
+  const input1Ref = useRef(null)
+  const input2Ref = useRef(null)
+  const input3Ref = useRef(null)
+  const input4Ref = useRef(null)
+  const input5Ref = useRef(null)
+
   const handleSumit = (e) => {
     e.preventDefault()
-    console.log('entra')
+    console.log(e)
   }
 
   const handleChange = (e) => {
@@ -18,17 +30,33 @@ export const Form = ({ dataCard, setDataCard }) => {
 
     if (nameInput === 'number') {
       const valueInpuArray = valueInput.split('')
-      if (valueInpuArray.length === 4 || valueInpuArray.length === 8 || valueInpuArray.length === 12) {
+      if (valueInpuArray.length === 4 || valueInpuArray.length === 9 || valueInpuArray.length === 14) {
         valueInpuArray.push('-')
       }
-      valueInput = valueInpuArray.join('')
-      console.log(valueInput)
+      valueInput = valueInpuArray.join('').replace('-', ' ')
+    }
+
+    if (nameInput === 'number' && valueInput.length === 20) {
+      return null
+    }
+
+    if (nameInput === 'month' && valueInput.length === 2) {
+      input4Ref.current.focus()
+    }
+
+    if (nameInput === 'year' && valueInput.length === 2) {
+      input5Ref.current.focus()
+    }
+
+    if (nameInput === 'code' && valueInput.length === 4) {
+      return null
     }
 
     const newData = {
-      ...dataCard,
+      ...dataCardRender,
       [e.target.id]: valueInput
     }
+
     setDataCardRender(newData)
     setDataCard(newData)
   }
@@ -37,13 +65,30 @@ export const Form = ({ dataCard, setDataCard }) => {
     <form onSubmit={(e) => handleSumit(e)} className='form-card'>
       <div className='form-card__name-container'>
         <label className='form-card__label' htmlFor='name'>CARDHOLDER NAME</label>
-        <input className='form-card__input form-card__input-long' type='text' id='name' name='name' placeholder='e.g Jane Appleseed' onChange={(e) => handleChange(e)} value={dataCardRender ? dataCardRender.name : ''} />
+        <input
+          className='form-card__input form-card__input-long'
+          type='text'
+          id='name'
+          name='name'
+          placeholder='e.g Jane Appleseed'
+          onChange={(e) => handleChange(e)}
+          value={dataCardRender ? dataCardRender.name : ''}
+          ref={input1Ref}
+        />
         <span className='form-card__error hidden'>Wrong format, numbers only</span>
       </div>
 
       <div className='form-card__number-container'>
         <label className='form-card__label' htmlFor='number'>CARD NUMBER</label>
-        <input className='form-card__input form-card__input-long' type='number' id='number' placeholder='e.g Jane Appleseed' onChange={(e) => handleChange(e)} value={dataCardRender ? dataCardRender.number : ''} />
+        <input
+          className='form-card__input form-card__input-long'
+          type='text'
+          id='number'
+          placeholder='e.g Jane Appleseed'
+          onChange={(e) => handleChange(e)}
+          value={dataCardRender ? dataCardRender.number : ''}
+          ref={input2Ref}
+        />
         <span className='form-card__error hidden'>Wrong format, numbers only</span>
       </div>
 
@@ -51,14 +96,38 @@ export const Form = ({ dataCard, setDataCard }) => {
         <div className='form-card__date-container'>
           <label className='form-card__label' htmlFor='month'>EXP. DATE (MM/YY)</label>
           <div className='form-card__info-date'>
-            <input className='form-card__input form-card__input-short' type='number' id='month' placeholder='MM' onChange={(e) => handleChange(e)} />
-            <input className='form-card__input form-card__input-short' type='number' id='year' placeholder='YY' onChange={(e) => handleChange(e)} />
+            <input
+              className='form-card__input form-card__input-short'
+              type='number'
+              id='month'
+              placeholder='MM'
+              onChange={(e) => handleChange(e)}
+              ref={input3Ref}
+            />
+            <input
+              className='form-card__input form-card__input-short'
+              type='number'
+              id='year'
+              placeholder='YY'
+              onChange={(e) => handleChange(e)}
+              ref={input4Ref}
+
+            />
           </div>
           <span className='form-card__error hidden'>Can’t be blank</span>
         </div>
         <div className='form-card__security-code-container'>
           <label className='form-card__label' htmlFor='code'>CVC</label>
-          <input className='form-card__input form-card__input-auto' type='number' id='code' placeholder='e.g. 123' onChange={(e) => handleChange(e)} />
+          <input
+            className='form-card__input form-card__input-auto'
+            type='number'
+            id='code'
+            placeholder='e.g. 123'
+            onChange={(e) => handleChange(e)}
+            ref={input5Ref}
+            value={dataCardRender ? dataCardRender.code : ''}
+
+          />
           <span className='form-card__error hidden'>Can’t be blank</span>
         </div>
       </div>
