8897516ded6115ce917952d1bec73c7bc7693e11
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-26 11:42:09 -0700

    🚀 [Add] Deployent configuration
    We add tools and script necessary to deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://jazzielp/github.io/interactive-card-details-form'
 })
